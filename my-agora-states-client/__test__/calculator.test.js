fa267e0ed1b027a19dcca9a04c6789e041f3a307
shimeeuisuk <euisuk95@gmail.com>
2022-08-08 15:42:15 +0900

    테스트 통과돼야해

--- my-agora-states-client/__test__/calculator.test.js
+++ my-agora-states-client/__test__/calculator.test.js
@@ -1,5 +1,5 @@
-import { Add } from "../src/components/calculator";
+const add = require("./Calculator");
 
 it("add correctly", () => {
-  expect(Add(3, 5)).toBe(15);
+  expect(add(3, 5)).toBe(8);
 });
--- my-agora-states-client/src/components/Calculator.js
+++
@@ -1,3 +0,0 @@
-export const Add = (a, b) => {
-  return a * b;
-};
